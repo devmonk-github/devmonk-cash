1306e829abb28063e1655e298afbc671da83e0d3
Ankitp1696 <ankit.patil@penpenny.com>
2022-03-21 20:18:22 +0100

    Transaction page layout implemantation

--- cash-register/src/app/transactions/transactions.component.ts
+++ cash-register/src/app/transactions/transactions.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { faLongArrowAltDown, faLongArrowAltUp, faMinusCircle, faPlus, faPlusCircle, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { ApiService } from '../shared/service/api.service';
+import { MenuComponent } from '../shared/_layout/components/common';
 // import {FreshChatService} from "../../shared/service/fresh-chat.service";
 // import {Employee} from "../../shared/models/employee.model";
 
@@ -10,6 +12,11 @@ import { ApiService } from '../shared/service/api.service';
 })
 export class TransactionsComponent implements OnInit {
   option: boolean = true;
+  faSearch= faSearch;
+  faIncrease = faPlusCircle;
+  faDecrease = faMinusCircle;
+  faArrowUp = faLongArrowAltUp;
+  faArrowDown = faLongArrowAltDown;
   cities = [
     {name: 'Amsterdam', code: 'AMS'},
     {name: 'Bruxelles', code: 'BRU'},
@@ -21,13 +28,34 @@ export class TransactionsComponent implements OnInit {
   businessDetails: any = {};
   userType: any = {};
   requestParams: any = {};
-  setPaginateSize: number = 1;
   widgetLog: string[] = [];
+  pageCounts: Array<number> = [ 10, 25, 50, 100]
+  pageCount: number = 10;
+  pageNumber: number = 1;
+  setPaginateSize: number = 1;
   paginationConfig: any = {
-    itemsPerPage: '1', 
+    itemsPerPage: '10', 
     currentPage: 1, 
     totalItems: 0
   };
+  showAdvanceSearch = false;
+  transactionMenu = [
+    {key : 'REST_PAYMENT'},
+    {key : 'REFUND/REVERT'},
+    {key : 'PREPAYMENT'},
+    {key : 'MARK_CONFIRMED'},
+  ];
+
+  tableHeaders: Array<any> = [
+    { key: 'Date', selected: false, sort: ''},
+    { key: 'Transaction no.', selected: false, sort: ''},
+    { key: 'Receipt number', selected: false, sort: ''},
+    { key: 'Customer', disabled:true},
+    { key: 'Method', disabled:true},
+    { key: 'Total', disabled:true},
+    { key: 'Type', disabled:true}
+  ]
+
 
   constructor(
     private apiService: ApiService
@@ -52,6 +80,9 @@ export class TransactionsComponent implements OnInit {
     this.apiService.postNew('cashregistry', '/api/v1/transaction/list', this.requestParams).subscribe((result: any) => {
       if (result && result.data && result.data.length && result.data[0] && result.data[0].result && result.data[0].result.length) {
         this.transactions = result.data[0].result;
+        setTimeout(()=>{
+          MenuComponent.bootstrap();
+        }, 1000);
       }
     }, (error) => {})
   }
@@ -64,8 +95,36 @@ export class TransactionsComponent implements OnInit {
     // this.chatService.closeWidget();
   }
 
+  // Function for handle event of transaction menu
+  clickMenuOpt(key: string, transactionId: string){
+
+  }
+
+  //  Function for set sort option on transaction table
+  setSortOption(sortHeader : any){
+    if(sortHeader.selected){
+      sortHeader.sort = sortHeader.sort == 'asc' ? 'desc' : 'asc';
+    } else {
+      this.tableHeaders = this.tableHeaders.map( (th : any) =>{
+        if(sortHeader.key == th.key){
+          th.selected = true;
+          th.sort = 'asc';
+        }else{
+          th.selected = false;
+        }
+        return th;
+      })
+    }
+  }
+
+  // Function for update item's per page
+  changeItemsPerPage(pageCount: any){
+    this.paginationConfig.itemsPerPage = pageCount;
+  }
+
+  // Function for trigger event after page changes
   pageChanged(page:any){
-    this.requestParams.skip = (page -1) * this.requestParams.limit;
+    this.requestParams.skip = (page -1) * parseInt(this.paginationConfig.itemsPerPage);
     this.loadTransaction();
     this.paginationConfig.currentPage = page;
   }
--- cash-register/src/app/transactions/transactions.module.ts
+++ cash-register/src/app/transactions/transactions.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { TransactionsRoutingModule } from './transactions-routing.module';
+import { NgxPaginationModule } from 'ngx-pagination';
 // import { SharedModule } from '../../shared/shared.module';
 
 import { TransactionsComponent } from './transactions.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @NgModule({
   declarations: [
     TransactionsComponent
   ],
   imports: [
     TransactionsRoutingModule,
-    CommonModule
+    FontAwesomeModule,
+    NgxPaginationModule,
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule
     // SharedModule
   ]
 })
