002424e69ae9b35f5b6a6fcb76fe89460f01bdaf
shubhamwade <shubamnwade@gmail.com>
2022-03-25 15:56:42 +0100

    Services page basic setup(similar to transaction)

--- cash-register/src/app/services/services.module.ts
+++ cash-register/src/app/services/services.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ServicesComponent } from './services.component';
 import { ServicesRoutingModule } from './services-routing.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @NgModule({
   declarations: [
@@ -9,7 +13,12 @@ import { ServicesRoutingModule } from './services-routing.module';
   ],
   imports: [
     CommonModule,
-    ServicesRoutingModule
+    ServicesRoutingModule,
+    FontAwesomeModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NgSelectModule,
+    NgxPaginationModule
   ]
 })
 export class ServicesModule { }
