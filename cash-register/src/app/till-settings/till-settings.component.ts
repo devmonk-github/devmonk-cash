1a70c50c773abe7a69d4f8f707312a6858518cf4
shubhamwade <shubamnwade@gmail.com>
2022-03-31 17:07:19 +0200

    Delete payment method functionality

--- cash-register/src/app/till-settings/till-settings.component.ts
+++ cash-register/src/app/till-settings/till-settings.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../shared/service/api.service';
 import { DialogService } from '../shared/service/dialog';
 import { CustomPaymentMethodComponent } from '../shared/components/custom-payment-method/custom-payment-method.component';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { ConfirmationDialogComponent } from '../shared/components/confirmation-dialog/confirmation-dialog.component';
 
 @Component({
   selector: 'app-till-settings',
@@ -10,8 +12,10 @@ import { CustomPaymentMethodComponent } from '../shared/components/custom-paymen
 })
 export class TillSettingsComponent implements OnInit {
 
+  faTrash = faTrash;
   payMethodsLoading: boolean = false;
   payMethods: Array<any> = [];
+  bookKeepingMode: boolean = false;
   requestParams: any = {
     iBusinessId: ''
   }
@@ -21,13 +25,39 @@ export class TillSettingsComponent implements OnInit {
     private dialogService: DialogService
   ) { }
 
+  deleteMethod(method: any){
+    const buttons = [
+      {text: "YES", value: true, status: 'success', class: 'btn-primary ml-auto mr-2'},
+      {text: "NO", value: false, class: 'btn-warning'}
+    ]
+    this.dialogService.openModal(ConfirmationDialogComponent, {
+      context: {
+        header: 'DELETE_PAYMENT_METHOD',
+        bodyText: 'ARE_YOU_SURE_TO_DELETE_THIS_PAYMENT_METHOD?',
+        buttonDetails: buttons
+      }
+    })
+      .instance.close.subscribe(
+      result => {
+        if (result) {
+           this.apiService.deleteNew('cashregistry', '/api/v1/payment-methods/remove/' + method._id + '?iBusinessId=' + this.requestParams.iBusinessId).subscribe((result: any) => {
+            this.getPaymentMethods()
+            }, (error) => {
+          })
+        }
+      }
+    )
+  }
+
   ngOnInit(): void {
     this.requestParams.iBusinessId = localStorage.getItem('currentBusiness');
     this.getPaymentMethods();
   }
 
   createPaymentMethod() {
-    this.dialogService.openModal(CustomPaymentMethodComponent, { cssClass:"", context: { mode: 'create' } }).instance.close.subscribe(result =>{ });
+    this.dialogService.openModal(CustomPaymentMethodComponent, { cssClass:"", context: { mode: 'create' } }).instance.close.subscribe(result =>{ 
+      this.getPaymentMethods();
+    });
   }
 
   close(){
@@ -36,6 +66,7 @@ export class TillSettingsComponent implements OnInit {
 
   getPaymentMethods(){
     this.payMethodsLoading = true;
+    this.payMethods = []
     this.apiService.getNew('cashregistry', '/api/v1/payment-methods/' + this.requestParams.iBusinessId + '?type=custom').subscribe((result: any) => {
       if (result && result.data && result.data.length) {
         this.payMethods = result.data;
