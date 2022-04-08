2d88e6996aaf3f36042cd2930deecb51e4186447
shubhamwade <shubamnwade@gmail.com>
2022-04-08 09:01:39 +0200

    Workstation settings WIP

--- cash-register/src/app/workstation/workstation.component.ts
+++ cash-register/src/app/workstation/workstation.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { faPencilAlt, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { ApiService } from '../shared/service/api.service';
+import { MenuComponent } from '../shared/_layout/components/common';
 
 @Component({
   selector: 'app-workstation',
@@ -9,6 +11,8 @@ import { ApiService } from '../shared/service/api.service';
 export class WorkstationComponent implements OnInit {
 
   addNew: boolean = false;
+  faPencilAlt = faPencilAlt;
+  faTrash = faTrash;
   workstation: any = {
     sName: '',
     sDescription: ''
@@ -16,6 +20,18 @@ export class WorkstationComponent implements OnInit {
   business: any = {};
   loading: boolean = false;
   workstations: Array<any> = [];
+  settings: Array<any> = [];
+
+  downloadOptions = [
+    {
+      title: 'CSV_DOWNLOAD',
+      key: 'CSV_DOWNLOAD'
+    },
+    {
+      title: 'PDF_DOWNLOAD',
+      key: 'PDF_DOWNLOAD'
+    }
+  ];
 
   constructor(
     private apiService: ApiService,
@@ -23,8 +39,55 @@ export class WorkstationComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    // MenuComponent.reinitialization();
     this.business._id = localStorage.getItem('currentBusiness');
     this.getWorkstations();
+  }
+
+  selectDropDown(key: String){
+    if (key){
+      switch (key){
+        case'CSV_DOWNLOAD':
+          this.getWorkstations();
+          break;
+        case'PDF_DOWNLOAD':
+          this.getWorkstations();
+          break;
+      }
+    }
+  }
+
+  getWorkstationSettings(iWorkstationId: any, i: number){
+    this.apiService.getNew('cashregistry', '/api/v1/workstation-settings/list/'+ this.business._id + '/' + iWorkstationId).subscribe(
+      (result : any) => {
+        this.settings = result.data;
+        this.workstations[i].settings = this.settings;
+      }),
+      (error: any) => {
+        this.loading = false;
+        console.error(error)
+      }
+  }
+
+  createWorkstationSettings(workstation: any){
+    const data = {
+      iBusinessId: this.business._id,
+      iLocationId: localStorage.getItem('currentLocation'),
+      iWorkstationId: workstation._id,
+      iDeviceId: '624e98bd8f532d15180f2d75'
+    }
+    this.workstation.iBusinessId = this.business._id;
+    this.loading = true;
+    this.addNew = false;
+    this.apiService.postNew('cashregistry', '/api/v1/workstation-settings/create', data).subscribe(
+      (result : any) => {
+        this.loading = false;
+        this.getWorkstations();
+      }),
+      (error: any) => {
+        this.loading = false;
+        console.error(error)
+      }
   }
 
   createWorkstation(){
@@ -38,7 +101,7 @@ export class WorkstationComponent implements OnInit {
       }),
       (error: any) => {
         this.loading = false;
-        console.log(error)
+        console.error(error)
       }
   }
 
@@ -46,8 +109,14 @@ export class WorkstationComponent implements OnInit {
     this.loading = true;
     this.apiService.getNew('cashregistry', '/api/v1/workstations/list/' + this.business._id).subscribe(
       (result : any) => {
-       if(result && result.data && result.data[0] && result.data[0].result && result.data[0].result.length){
-        this.workstations = result.data[0].result;
+       if(result && result.data){
+        this.workstations = result.data;
+        for(let i = 0; i < this.workstations.length; i++){
+          this.getWorkstationSettings(this.workstations[i]._id, i);
+        }
+        setTimeout(() => {
+          MenuComponent.createInstances('[data-kt-menu="true"]');
+        }, 1000);;
        }       
         this.loading = false;
       }),
