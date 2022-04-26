c6fb8167a138b5df5f5ec8275471f8b6d20c9bdb
Niek <niek@prismanote.com>
2022-04-26 16:34:27 +0200

    Making the PDF function niver

--- cash-register/src/app/shared/components/pdf/pdf.component.ts
+++ cash-register/src/app/shared/components/pdf/pdf.component.ts
@@ -1,27 +1,15 @@
-import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-pdf',
-  template: '<div #pdfGenerator id="pdfGenerator"></div><iframe [style]="frameStyle" #pdfHtmlFrame id="pdfHtmlFrame">{{pdfString}}</iframe>',
+  template: '<div #pdfGenerator id="pdfGenerator"></div>',
   styleUrls: ['./pdf.component.sass']
 })
 export class PdfComponent implements OnInit {
-  @Input() pdfString: string = ""
-  @ViewChild('pdfHtmlFrame') pdfHtmlFrame!: ElementRef;
-  frameStyle: string = "";
+
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  setHtmlContent(content: string): void {
-    let doc = this.pdfHtmlFrame.nativeElement.contentDocument || this.pdfHtmlFrame.nativeElement.document;
-    doc.body.innerHTML = content;
-  }
-
-  setStyle(pageSize: any) {
-    this.frameStyle = 'width: ' + pageSize.width + 'mm; height: ' + pageSize.height + 'mm';
-    // console.log('frameStyle', this.frameStyle)
   }
 
 }
