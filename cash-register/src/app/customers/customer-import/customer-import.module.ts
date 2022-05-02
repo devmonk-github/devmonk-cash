34c78d422441cd8f7ef2592c3a14a645b9a1a359
shubhamwade <shubhamnwade@gmail.com>
2022-05-02 21:17:40 +0200

    Browser animation module issue update

--- cash-register/src/app/customers/customer-import/customer-import.module.ts
+++ cash-register/src/app/customers/customer-import/customer-import.module.ts
@@ -1,13 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CustomerImportComponent } from './customer-import.component';
+import { FileUploadModule } from '@iplab/ngx-file-upload';
+import { BrowserAnimationsModule   } from '@angular/platform-browser/animations';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FileImportComponent } from '../file-import/file-import.component';
+import { CustomerDetailsImportComponent } from '../customer-details-import/customer-details-import.component';
+import { CustomerImportRoutingModule } from './customer-import-routing.module';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { TranslateModule } from '@ngx-translate/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    CustomerImportComponent
+    CustomerImportComponent,
+    FileImportComponent,
+    CustomerDetailsImportComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    CustomerImportRoutingModule,
+    ReactiveFormsModule,
+    NgxPaginationModule,
+    TranslateModule,
+    FormsModule,
+    FileUploadModule,
+    BrowserAnimationsModule,
+    FontAwesomeModule
   ]
 })
 export class CustomerImportModule { }
