cf0d4dffa57d2ff64024835024b28286b37d20d7
Niek <niek@prismanote.com>
2022-05-13 11:24:53 +0200

    (WIP) Added barcode service

--- cash-register/src/app/barcode/barcode.component.ts
+++ cash-register/src/app/barcode/barcode.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {BarcodeService} from "../shared/service/barcode.service";
 
 @Component({
   selector: 'app-barcode',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BarcodeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private barcodeScanner: BarcodeService) { }
+  barcode: string = ""
+  scannerUsed: boolean = false
+
+
 
   ngOnInit(): void {
+    this.barcodeScanner.barcodeScanned.subscribe( (barcode: string) => {
+      this.barcode = barcode
+      this.scannerUsed = true
+    })
   }
+
+  onInputChange(): void {
+    this.scannerUsed = false
+  }
+
 
 }
