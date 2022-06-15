d02caedbd1f2aec30c89e67840667cb0f9b0b785
whotushardattu <tushardattu@gmail.com>
2022-06-15 14:20:52 +0530

    payment distribution issue fixed

--- cash-register/src/app/shared/service/payment-distribution.service.ts
+++ cash-register/src/app/shared/service/payment-distribution.service.ts
@@ -18,35 +18,30 @@ export class PaymentDistributionService {
   distributeAmount(transactionItems: any[], availableAmount: any): any[] {
     transactionItems.map((i: any) => {
       i.amountToBePaid = i.price * i.quantity - (i.prePaidAmount || 0);
-      // if (i.tType && i.tType === 'refund') {
-      //   availableAmount += i.prePaidAmount;
-      //   if (i.amountToBePaid === 0) {
-      //     i.amountToBePaid = -1 * i.prePaidAmount;
-      //   }
-      // }
-      // if (i.paymentAmount > i.amountToBePaid) {
-      //   i.paymentAmount = i.amountToBePaid;
-      // };
-      // return i;
+      if (i.tType && i.tType === 'refund') {
+        // availableAmount += i.prePaidAmount;
+        // if (i.amountToBePaid === 0) {
+        i.amountToBePaid = -1 * i.prePaidAmount;
+        // }
+      }
+      if (i.paymentAmount > i.amountToBePaid) {
+        i.paymentAmount = i.amountToBePaid;
+      };
+      return i;
     });
-    console.log(availableAmount);
-
     const setAmount = transactionItems.filter(item => item.isExclude);
     setAmount.map(i => (i.paymentAmount = 0));
 
     const arrToUpdate = transactionItems.filter(item => !item.manualUpdate && !item.isExclude);
-
     const arrNotToUpdate = transactionItems.filter(item => item.manualUpdate && !item.isExclude);
     const assignedAmountToManual = arrNotToUpdate.reduce((n, { paymentAmount }) => n + paymentAmount, 0);
     availableAmount -= assignedAmountToManual;
 
     if (arrToUpdate.length > 0) {
       const totalAmountToBePaid = arrToUpdate.reduce((n, { amountToBePaid }) => n + amountToBePaid, 0);
-      console.log(totalAmountToBePaid);
       if (totalAmountToBePaid !== 0) {
-        arrToUpdate.map(i => (i.paymentAmount = this.roundToXDigits(i.amountToBePaid * availableAmount / Math.abs(totalAmountToBePaid))));
+        arrToUpdate.map(i => (i.paymentAmount = this.roundToXDigits(i.amountToBePaid * availableAmount / totalAmountToBePaid)));
       }
-      console.log(arrToUpdate);
       const assignedAmount = arrToUpdate.reduce((n, { paymentAmount }) => n + paymentAmount, 0);
       arrToUpdate[arrToUpdate.length - 1].paymentAmount += (availableAmount - assignedAmount);
     }
@@ -55,7 +50,6 @@ export class PaymentDistributionService {
         element.paymentAmount = element.nTotal;
       }
     });
-    console.log(transactionItems);
     return transactionItems;
   }
 
