ee66181e56b5021a069483581ce43bd8bb6bb6a4
Ankitp1696 <ankit.patil@penpenny.com>
2022-06-16 15:28:03 +0530

    Update environment variables

--- cash-register/src/environments/environment.prod.ts
+++ cash-register/src/environments/environment.prod.ts
@@ -1,6 +1,11 @@
 export const environment = {
   production: true,
-  apiBasePath: 'http://54.246.32.179',
+  CORE_URL: 'https://core.e-orderportal.com',
+  CASH_URL: 'https://cashregister.e-orderportal.com',
+  AUTH_URL: 'https://auth.e-orderportal.com',
+  CUSTOMER_URL: 'https://customer.e-orderportal.com',
+  BOOKKEEPING_URL: 'https://bookkeeping.e-orderportal.com',
+  BACKUP_URL: 'https://backup.e-orderportal.com',
   apiURL: 'http://prismanote.com',
   oldPlatformUrl: 'https://kassa.prismanote.com',
 };
