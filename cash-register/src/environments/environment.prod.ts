f52d759a1222a0b6487d129410165541e234e42e
Naimish Kher <naimishkher95>
2022-06-27 19:09:38 +0530

    environment file updated

--- cash-register/src/environments/environment.prod.ts
+++ cash-register/src/environments/environment.prod.ts
@@ -5,7 +5,12 @@ export const environment = {
   AUTH_URL: 'https://auth.e-orderportal.com',
   CUSTOMER_URL: 'https://customer.e-orderportal.com',
   BOOKKEEPING_URL: 'https://bookkeeping.e-orderportal.com',
+  WEBSITE_URL: 'https://website.e-orderportal.com',
   BACKUP_URL: 'https://backup.e-orderportal.com',
-  apiURL: 'http://prismanote.com',
-  oldPlatformUrl: 'https://kassa.prismanote.com',
+  ORGANIZATION_URL: 'https://organization.e-orderportal.com',
+  apiBasePath: 'e-orderportal.com',
+  apiURL: 'http://localhost:3000',
+  encryptionKey: '2e35f242a46d67eeb74aabc37d5e5d05',
+  oldPlatformUrl: `https://kassa.prismanote.com`,
+  indexedDBName: 'prismaNote'
 };
