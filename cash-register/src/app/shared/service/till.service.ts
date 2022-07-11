2911d8e417aa446d922d9acb207782301f6db612
whotushardattu <tushardattu@gmail.com>
2022-07-11 15:25:39 +0530

    workstationId conflict resoled

--- cash-register/src/app/shared/service/till.service.ts
+++ cash-register/src/app/shared/service/till.service.ts
@@ -97,7 +97,7 @@ export class TillService {
     const body = {
       iBusinessId: this.getValueFromLocalStorage('currentBusiness'),
       iLocationId: this.getValueFromLocalStorage('currentLocation'),
-      iWorkstationId: this.getValueFromLocalStorage('currentWorkstation'),
+      iDeviceId: this.getValueFromLocalStorage('currentWorkstation'),
       transactionItems: transactionItems,
       oTransaction: transaction,
       payments: this.getUsedPayMethods(false, payMethods),
