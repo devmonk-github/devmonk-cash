c34036105a4a2cc38544b8b745e8ddb1aec3111c
shubhamwade <shubhamnwade@gmail.com>
2022-07-21 21:04:10 +0530

    Activity items page WIP

--- cash-register/src/app/activity-items/activity-items.component.ts
+++ cash-register/src/app/activity-items/activity-items.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../shared/service/api.service';
 
 @Component({
   selector: 'app-activity-items',
@@ -7,9 +8,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActivityItemsComponent implements OnInit {
 
-  constructor() { }
+  pageCounts: Array<number> = [10, 25, 50, 100]
+  pageCount: number = 10;
+  pageNumber: number = 1;
+  setPaginateSize: number = 1;
+  paginationConfig: any = {
+    itemsPerPage: '10',
+    currentPage: 1,
+    totalItems: 0
+  };
+  businessDetails: any = {};
+  iLocationId: String | null | undefined;
+  requestParams: any = {
+    searchValue: '',
+    sortBy: { key: 'Date', selected: true, sort: 'asc' },
+    sortOrder: 'asc'
+  };
+  activityItems: Array<any> = [];
+  showLoader: Boolean = false;
+
+  constructor(
+    private apiService: ApiService
+  ) { }
 
   ngOnInit(): void {
+    this.businessDetails._id = localStorage.getItem('currentBusiness');
+    this.iLocationId = localStorage.getItem('currentLocation');
+    this.loadTransaction();
+  }
+
+  loadTransaction() {
+    this.activityItems = [];
+    this.requestParams.iBusinessId = this.businessDetails._id;
+    this.requestParams.skip = this.requestParams.skip || 0;
+    this.requestParams.limit = this.paginationConfig.itemsPerPage || 50;
+    if(this.iLocationId) this.requestParams.iLocationId = this.iLocationId;
+    this.showLoader = true;
+    this.apiService.postNew('cashregistry', '/api/v1/activities', this.requestParams).subscribe(
+      (result: any) => {
+        console.log(result);
+        this.activityItems = result.data;
+        console.log(this.activityItems);
+        this.paginationConfig.totalItems = result.count;
+        // setTimeout(() => {
+        //   MenuComponent.bootstrap();
+        // }, 1000);
+        this.showLoader = false;
+      }, 
+      (error: any) => {
+        this.showLoader = false;
+      })
   }
 
 }
