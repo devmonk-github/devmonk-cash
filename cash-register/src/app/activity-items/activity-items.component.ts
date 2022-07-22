55d3a2baeb2fad7523c65887ad6af1cb717f8899
shubhamwade <shubhamnwade@gmail.com>
2022-07-22 12:36:03 +0530

    Activity items page WIP

--- cash-register/src/app/activity-items/activity-items.component.ts
+++ cash-register/src/app/activity-items/activity-items.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { faLongArrowAltDown, faLongArrowAltUp, faMinusCircle, faPlusCircle, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { ApiService } from '../shared/service/api.service';
+import { MenuComponent } from '../shared/_layout/components/common';
 
 @Component({
   selector: 'app-activity-items',
@@ -26,6 +28,46 @@ export class ActivityItemsComponent implements OnInit {
   };
   activityItems: Array<any> = [];
   showLoader: Boolean = false;
+  faSearch = faSearch;
+  faIncrease = faPlusCircle;
+  faDecrease = faMinusCircle;
+  faArrowUp = faLongArrowAltUp;
+  faArrowDown = faLongArrowAltDown;
+
+  locations: Array<any> = [];
+  selectedLocations: Array<any> = [];
+  showAdvanceSearch = false;
+
+  workstations: Array<any> = [];
+  selectedWorkstations: Array<any> = [];
+
+  employee: any = { sFirstName: 'All' };
+  employees: Array<any> = [this.employee];
+
+  filterDates: any = {
+    endDate: new Date(new Date().setHours(23, 59, 59)),
+    startDate: new Date('01-01-2015'),
+  }
+
+  paymentMethods: Array<any> = ['All', 'Cash', 'Credit', 'Card', 'Gift-Card'];
+  transactionTypes: Array<any> = ['All', 'Refund', 'Repair', 'Gold-purchase', 'Gold-sale'];
+  transactionStatus: string = 'all';
+  invoiceStatus: string = 'all';
+  importStatus: string = 'all';
+  methodValue: string = 'All';
+  transactionValue: string = 'All';
+
+  tableHeaders: Array<any> = [
+    { key: 'Activity No.', selected: false, sort: '' },
+    { key: 'Repair number', disabled: true },
+    { key: 'Type', disabled: true },
+    { key: 'Intake date', selected: true, sort: 'asc' },
+    { key: 'End date', selected: false, sort: 'asc' },
+    { key: 'Status', disabled: true },
+    { key: 'Supplier/Repairer', disabled: true },
+    { key: 'Partner supplier status', disabled: true },
+    { key: 'Customer', disabled: true },
+  ]
 
   constructor(
     private apiService: ApiService
@@ -40,24 +82,66 @@ export class ActivityItemsComponent implements OnInit {
   loadTransaction() {
     this.activityItems = [];
     this.requestParams.iBusinessId = this.businessDetails._id;
-    this.requestParams.skip = this.requestParams.skip || 0;
     this.requestParams.limit = this.paginationConfig.itemsPerPage || 50;
     if(this.iLocationId) this.requestParams.iLocationId = this.iLocationId;
     this.showLoader = true;
-    this.apiService.postNew('cashregistry', '/api/v1/activities', this.requestParams).subscribe(
+    this.apiService.postNew('cashregistry', '/api/v1/activities/items', this.requestParams).subscribe(
       (result: any) => {
         console.log(result);
         this.activityItems = result.data;
         console.log(this.activityItems);
         this.paginationConfig.totalItems = result.count;
-        // setTimeout(() => {
-        //   MenuComponent.bootstrap();
-        // }, 1000);
+        console.log(this.paginationConfig);
+        setTimeout(() => {
+          MenuComponent.bootstrap();
+        }, 1000);
         this.showLoader = false;
       }, 
       (error: any) => {
         this.showLoader = false;
       })
+  }
+
+   // Function for update item's per page
+   changeItemsPerPage(pageCount: any) {
+    this.paginationConfig.itemsPerPage = pageCount;
+    this.loadTransaction();
+  }
+
+  // Function for trigger event after page changes
+  pageChanged(page: any) {
+    this.requestParams.skip = (page - 1) * parseInt(this.paginationConfig.itemsPerPage);
+    this.loadTransaction();
+    this.paginationConfig.currentPage = page;
+  }
+
+  sortAndLoadTransactions(sortHeader: any) {
+    let sortBy = 'dCreatedDate';
+    if (sortHeader.key == 'End date') sortBy = 'dPickedUp';
+    if (sortHeader.key == 'Intake date') sortBy = 'dCreatedDate';
+    if (sortHeader.key == 'Activity No.') sortBy = 'sNumber';
+    this.requestParams.sortBy = sortBy;
+    this.requestParams.sortOrder = sortHeader.sort;
+    this.loadTransaction();
+  }
+
+   //  Function for set sort option on transaction table
+   setSortOption(sortHeader: any) {
+    if (sortHeader.selected) {
+      sortHeader.sort = sortHeader.sort == 'asc' ? 'desc' : 'asc';
+      this.sortAndLoadTransactions(sortHeader)
+    } else {
+      this.tableHeaders = this.tableHeaders.map((th: any) => {
+        if (sortHeader.key == th.key) {
+          th.selected = true;
+          th.sort = 'asc';
+        } else {
+          th.selected = false;
+        }
+        return th;
+      })
+      this.sortAndLoadTransactions(sortHeader)
+    }
   }
 
 }
--- cash-register/src/app/activity-items/activity-items.module.ts
+++ cash-register/src/app/activity-items/activity-items.module.ts
@@ -2,14 +2,28 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivityItemsComponent } from './activity-items.component';
 import { ActivityItemsRoutingModule } from './activity-items-routing.module';
+import { TranslateModule } from '@ngx-translate/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { NgxPaginationModule, PaginatePipe } from 'ngx-pagination';
 
 @NgModule({
   declarations: [
     ActivityItemsComponent
   ],
   imports: [
+    TranslateModule,
     CommonModule,
-    ActivityItemsRoutingModule
+    ActivityItemsRoutingModule,
+    FontAwesomeModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NgSelectModule,
+    NgxPaginationModule
+  ],
+  providers: [
+    PaginatePipe
   ]
 })
 export class ActivityItemsModule { }
