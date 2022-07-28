fb4affff20580ec06cec744ecc394294878cad28
krtalshania <krtalshania2022@gmail.com>
2022-07-28 18:55:30 +0530

    moved day-closure to cashregister

--- cash-register/src/app/day-closure/day-closure.component.ts
+++ cash-register/src/app/day-closure/day-closure.component.ts
@@ -1,9 +1,9 @@
 import { Subscription } from 'rxjs';
 import { Component, OnInit, HostListener, ViewChild, OnDestroy } from '@angular/core';
+
+import { ActivatedRoute, Router } from '@angular/router';
 import { ApiService } from '../shared/service/api.service';
 import { ToastService } from '../shared/components/toast/toast.service';
-
-
 
 @Component({
   selector: 'app-day-closure',
@@ -16,10 +16,13 @@ export class DayClosureComponent implements OnInit, OnDestroy {
   iLocationId: any;
   iWorkstationId: any;
   closingDayState: boolean = false;
+  bIsDayStateOpened: boolean = false;
   closeSubscription !: Subscription;
   constructor(
     private apiService: ApiService,
-    private toastService: ToastService
+    private toastService: ToastService,
+    private router: Router,
+    private route: ActivatedRoute
   ) { }
 
 
@@ -38,7 +41,7 @@ export class DayClosureComponent implements OnInit, OnDestroy {
     this.iBusinessId = localStorage.getItem('currentBusiness');
     this.iLocationId = localStorage.getItem('currentLocation');
     this.iWorkstationId = localStorage.getItem('currentWorkstation');
-    // this.closeDayState();
+    this.isAnyDayStateOpened();
   }
 
   onFrameLoad(): void {
@@ -48,28 +51,45 @@ export class DayClosureComponent implements OnInit, OnDestroy {
     iWindow.postMessage({ "parentOrigin": window.location.origin }, 'http://localhost:4002');
   }
 
-  closeDayState(event?: any) {
+  /*   closeDayState(event?: any) {
+      const oBody = {
+        iBusinessId: this.iBusinessId,
+        iLocationId: this.iLocationId,
+        iWorkstationId: this.iWorkstationId
+      }
+      this.closingDayState = true;
+      if (event) event.target.disabled = true;
+      this.closeSubscription = this.apiService.postNew('cashregistry', `/api/v1/statistics/close/day-state`, oBody).subscribe((result: any) => {
+        this.toastService.show({ type: 'success', text: `Day-state is close now` });
+        this.closingDayState = false;
+        if (event) event.target.disabled = false;
+      }, (error) => {
+        console.log('Error: ', error);
+        this.toastService.show({ type: 'warning', text: 'Something went wrong or open the day-state first' });
+        this.closingDayState = false;
+        if (event) event.target.disabled = false;
+      })
+    } */
+
+  ngOnDestroy(): void {
+    if (this.closeSubscription) this.closeSubscription.unsubscribe();
+  }
+
+  isAnyDayStateOpened() {
     const oBody = {
       iBusinessId: this.iBusinessId,
       iLocationId: this.iLocationId,
       iWorkstationId: this.iWorkstationId
     }
-    this.closingDayState = true;
-    if (event) event.target.disabled = true;
-    this.closeSubscription = this.apiService.postNew('cashregistry', `/api/v1/statistics/close/day-state`, oBody).subscribe((result: any) => {
-      this.toastService.show({ type: 'success', text: `Day-state is close now` });
-      this.closingDayState = false;
-      if (event) event.target.disabled = false;
+    this.apiService.postNew('cashregistry', `/api/v1/statistics/day-closure/check`, oBody).subscribe((result: any) => {
+      if (result?.data?.bIsDayStateOpened && result?.data?.oStatisticDetail?._id) {
+        this.bIsDayStateOpened = true;
+        console.log('isAnyDayStateOpened: ', this.bIsDayStateOpened, result.data.oStatisticDetail._id);
+        this.router.navigate(['../transactions-audit/view', result.data.oStatisticDetail._id], { relativeTo: this.route })
+      }
     }, (error) => {
-      console.log('Error: ', error);
-      this.toastService.show({ type: 'warning', text: 'Something went wrong or open the day-state first' });
-      this.closingDayState = false;
-      if (event) event.target.disabled = false;
+      console.error('Error here: ', error);
+      this.toastService.show({ type: 'warning', text: `Day-state is not closed` });
     })
-  }
-
-  ngOnDestroy(): void {
-    if (this.closeSubscription) this.closeSubscription.unsubscribe();
-
   }
 }
