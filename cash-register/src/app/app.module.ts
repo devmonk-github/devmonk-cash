092fa1696acc9231743557538f671106ae913ba6
Aditya1942 <aditya.devmonk@gmail.com>
2022-09-28 16:19:25 +0530

    CustomTranslateLoader removed

--- cash-register/src/app/app.module.ts
+++ cash-register/src/app/app.module.ts
@@ -11,24 +11,10 @@ import { FormsModule } from "@angular/forms";
 import { Observable } from 'rxjs';
 
 // Translate imports
-import * as en from '../assets/json/translations/en-translation.json';
-import * as nl from '../assets/json/translations/nl-translation.json';
 import { NgJsonEditorModule } from 'ang-jsoneditor';
 import { SharedServiceModule } from './shared/shared-service.module';
 import { BarcodeComponent } from './barcode/barcode.component';
-export class CustomTranslateLoader implements TranslateLoader {
-  public getTranslation(lang: string): Observable<any> {
-    return new Observable((observer: any) => {
-      lang = localStorage.getItem('language') || 'nl';
-      if (lang === 'nl') {
-        observer.next(nl);
-      } else {
-        observer.next(en);
-      }
-      observer.complete();
-    });
-  }
-}
+
 
 @NgModule({
   declarations: [
