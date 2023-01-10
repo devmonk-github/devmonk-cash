5b90cc6ca90b915f83f03f3beabb82f13cee5b30
krtalshania2022 <krtalshania2022@gmail.com>
2023-01-10 12:24:01 +0530

    quick button settings update

--- src/app/shared/components/add-favourites/favourites.component.ts
+++ src/app/shared/components/add-favourites/favourites.component.ts
@@ -52,6 +52,7 @@ export class AddFavouritesComponent implements OnInit {
   ];
   iLocationId: string = '';
   translation:any =[];
+  button?:any;
 
   constructor(
     private viewContainer: ViewContainerRef,
@@ -70,6 +71,11 @@ export class AddFavouritesComponent implements OnInit {
     this.translationService.get(translate).subscribe((res:any)=>{
       this.translation = res;
     })
+
+    if(this.mode==='edit') {
+      console.log(this.button)
+      this.onSelectProduct(this.button)
+    }
   } 
 
   async search() {
@@ -96,85 +102,24 @@ export class AddFavouritesComponent implements OnInit {
     if (shopResult && shopResult.data && shopResult.data.length) {
       this.shopProducts = shopResult.data[0].result;
     }
-    // const commonResult:any = await this.apiService.postNew('core', '/api/v1/products/commonbrand/list', data).toPromise();
-    // if (commonResult && commonResult.data && commonResult.data.length) {
-    //   const response = commonResult.data[0];
-    //   this.commonProducts = response.result;
-    // }
     this.searching = false;
-    // this.listShopProducts(this.searchKeyword, false);
-    // if (!this.isStockSelected) {
-      // this.listCommonBrandProducts(this.searchKeyword, false); // Searching for the products of common brand
-    // }
   }
   
 
-  /* Search API for finding the  common-brands products */
-  // listShopProducts(searchValue: string | undefined, isFromEAN: boolean | false) {
-  //   let data = {
-  //     iBusinessId: this.business._id,
-  //     skip: 0,
-  //     limit: 10,
-  //     sortBy: '',
-  //     sortOrder: '',
-  //     searchValue: searchValue,
-  //     aProjection: this.aProjection,
-  //     oFilterBy: {
-  //       oStatic: {},
-  //       oDynamic: {}
-  //     }
-  //   }
-  //   this.searching = true;
-  //   this.apiService.postNew('core', '/api/v1/business/products/list', data).subscribe((result: any) => {
-  //     this.searching = false;
-  //     if (result && result.data && result.data.length) {
-  //       const response = result.data[0];
-  //       this.shopProducts = response.result;
-  //     }
-  //   }, (error) => {
-
-  //   });
-  // }
-
-  // listCommonBrandProducts(searchValue: string | undefined, isFromEAN: boolean | false) {
-  //   try {
-  //     let data = {
-  //       iBusinessId: this.business._id,
-  //       skip: 0,
-  //       limit: 10,
-  //       sortBy: '',
-  //       sortOrder: '',
-  //       searchValue: searchValue,
-  //       aProjection: this.aProjection,
-  //       oFilterBy: {
-  //         oStatic: {},
-  //         oDynamic: {}
-  //       }
-  //     };
-  //     this.searching = true;
-  //     this.apiService.postNew('core', '/api/v1/products/commonbrand/list', data).subscribe((result: any) => {
-  //       this.searching = false;
-  //       if (result && result.data && result.data.length) {
-  //         const response = result.data[0];
-  //         this.commonProducts = response.result;
-  //       }
-  //     }, (error) => {
-  //       this.searching = false;
-  //     })
-  //   } catch (e) {
-  //     this.searching = false;
-  //   }
-  // }
-
-  onSelectProduct(product: any, isFrom: string, isFor: string) {
-    this.newSelectedProduct.sName = product.oName ? product.oName['en'] : 'No name';
-    this.newSelectedProduct.iBusinessProductId = product._id;
-    this.newSelectedProduct.aImage = product.aImage;
-    if (product?.aLocation?.length){
-      this.newSelectedProduct.nPrice = product?.aLocation.filter((location: any) => location._id === this.iLocationId)[0]?.nPriceIncludesVat || 0
+  onSelectProduct(product: any, isFrom?: string, isFor?: string) {
+    if (this.mode === 'create') {
+      this.newSelectedProduct.sName = product.oName ? product.oName['en'] : 'No name';
+      this.newSelectedProduct.iBusinessProductId = product._id;
+      this.newSelectedProduct.aImage = product.aImage;
+      if (product?.aLocation?.length) {
+        this.newSelectedProduct.nPrice = product?.aLocation.filter((location: any) => location._id === this.iLocationId)[0]?.nPriceIncludesVat || 0
+      }
+      this.shopProducts = null;
+    } else {
+      this.newSelectedProduct.nPrice = product?.nPrice || 0;
+      this.newSelectedProduct.sName = product?.sName || 'No name';
+      this.newSelectedProduct._id = product?._id;
     }
-
-    this.shopProducts = null;
   }
 
   assignProduct(){
@@ -202,36 +147,32 @@ export class AddFavouritesComponent implements OnInit {
 
   }
 
-  create(event: any) {
+  async create(event: any) {
     event.target.disabled = true;
     this.creating = true;
 
-    try {
-      let data = {
-        iBusinessId: this.business._id,
-        iLocationId: localStorage.getItem('currentLocation'),
-        oQuickButton: this.newSelectedProduct
-      };
+    let data = {
+      iBusinessId: this.business._id,
+      iLocationId: localStorage.getItem('currentLocation'),
+      oQuickButton: this.newSelectedProduct
+    };
 
-      if (!data.iLocationId) {
-        this.toastService.show({ type: 'warning', text: `Please select a location` });
-        return;
-      }
+    if (!data.iLocationId) {
+      this.toastService.show({ type: 'warning', text: `Please select a location` });
+      return;
+    }
+    let _result: any;
+    if (this.mode === 'create') {
+      _result = await this.apiService.postNew('cashregistry', '/api/v1/quick-buttons/create', data).toPromise();
+    } else {
+      _result = await this.apiService.putNew('cashregistry', `/api/v1/quick-buttons/${this.newSelectedProduct._id}`, data).toPromise();
+    }
+    event.target.disabled = false;
+    this.creating = false;
 
-      this.apiService.postNew('cashregistry', '/api/v1/quick-buttons/create', data).subscribe((result: any) => {
-        if (result.message == 'success') {
-          event.target.disabled = false;
-          this.creating = false;
-          this.close(true);
-          this.toastService.show({ type: 'success', text: `New Quick Button added successfully` });
-        }
-      }, (error) => {
-        event.target.disabled = false;
-        this.creating = false;
-      })
-    } catch (e) {
-      event.target.disabled = false;
-      this.creating = false;
+    if (_result.message == 'success') {
+      this.close(true);
+      this.toastService.show({ type: 'success', text: `New Quick Button added successfully` });
     }
   }
 
