25c277739360d8e6c9b518b067b7c935a2f45d48
Naimish Kher <naimishkher95@gmail.com>
2023-02-08 19:19:54 +0530

    CSP added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,17 +1,38 @@
+const CORE_URL = 'https://core.e-orderportal.com';
+const CASH_URL = 'https://cashregister.e-orderportal.com';
+const AUTH_URL = 'https://auth.e-orderportal.com';
+const CUSTOMER_URL = 'https://customer.e-orderportal.com';
+const BOOKKEEPING_URL = 'https://bookkeeping.e-orderportal.com';
+const WEBSITE_URL = 'https://website.e-orderportal.com';
+const FISKALY_URL = 'http://localhost:3020';
+const BACKUP_URL = 'https://backup.e-orderportal.com';
+const ORGANIZATION_URL = 'https://organization.e-orderportal.com';
+const LOG_URL = 'https://log.e-orderportal.com';
+
 export const environment = {
   production: true,
-  CORE_URL: 'https://core.e-orderportal.com',
-  CASH_URL: 'https://cashregister.e-orderportal.com',
-  AUTH_URL: 'https://auth.e-orderportal.com',
-  CUSTOMER_URL: 'https://customer.e-orderportal.com',
-  BOOKKEEPING_URL: 'https://bookkeeping.e-orderportal.com',
-  WEBSITE_URL: 'https://website.e-orderportal.com',
-  FISKALY_URL: 'http://localhost:3020',
-  BACKUP_URL: 'https://backup.e-orderportal.com',
-  ORGANIZATION_URL: 'https://organization.e-orderportal.com',
-  LOG_URL: 'https://log.e-orderportal.com',
+  CORE_URL: CORE_URL,
+  CASH_URL: CASH_URL,
+  AUTH_URL: AUTH_URL,
+  CUSTOMER_URL: CUSTOMER_URL,
+  WEBSITE_URL: WEBSITE_URL,
+  BOOKKEEPING_URL: BOOKKEEPING_URL,
+  BACKUP_URL: BACKUP_URL,
+  ORGANIZATION_URL: ORGANIZATION_URL,
+  LOG_URL: LOG_URL,
+  FISKALY_URL: FISKALY_URL,
+
   apiURL: 'http://prismanote.com',
   oldPlatformUrl: 'https://kassa.prismanote.com',
   fiskalyURL: 'https://kassensichv-middleware.fiskaly.com/api/v2',
-  indexedDBName: 'prismaNote'
+  indexedDBName: 'prismaNote',
+  csp: `
+    default-src 'self';
+    img-src https://* 'self' data:;
+    font-src 'self' https://fonts.gstatic.com;
+    style-src 'self' wchat.eu.freshchat.com https://fonts.googleapis.com https://fonts.googleapis.com 'unsafe-inline';
+    script-src 'self' https://wchat.eu.freshchat.com ${ORGANIZATION_URL};
+    connect-src 'self' ${CORE_URL} ${CASH_URL} ${AUTH_URL} ${CUSTOMER_URL} ${WEBSITE_URL} ${BOOKKEEPING_URL} ${BACKUP_URL} ${ORGANIZATION_URL} ${LOG_URL} ${FISKALY_URL};
+    frame-src 'self' https://wchat.eu.freshchat.com https://533155761215573.eu.webpush.freshchat.com
+  `
 };
