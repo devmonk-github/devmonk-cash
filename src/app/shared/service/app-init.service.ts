e63bbff69d058b95fa01c653919b6317751c9ea8
Naimish Kher <naimishkher95@gmail.com>
2023-02-10 12:50:09 +0530

    iFrame removed, CSP config added and debugging eval in thermal print

---
+++ src/app/shared/service/app-init.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class AppInitService {
+
+    constructor() { }
+
+    showError(dynamicMessage?: string) {
+        const initializationError = document.querySelector('#initializationError');
+        const message = 'Aw, something went wrong';
+        if (initializationError) initializationError.textContent = dynamicMessage || message;
+    }
+
+    initCsp() {
+        const headCsp = document.querySelector('head');
+        if (headCsp) headCsp.innerHTML += `<meta http-equiv="Content-Security-Policy" content="${environment.csp}">`;
+        else this.showError(`Something went wrong. Contact to RetailGear Team.`);
+    }
+}
