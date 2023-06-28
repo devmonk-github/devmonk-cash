86831b5a4e59ee184f18c2b33298b8882be90c89
EmanuelaG <emanuelaretailgear@gmail.com>
2023-06-28 14:54:13 +0200

    FIX bag number prefix undefined FIX update bag number and prefix in till settings (if aBagNumbers is empty)

--- src/app/till/order/order.component.ts
+++ src/app/till/order/order.component.ts
@@ -175,7 +175,7 @@ export class OrderComponent implements OnInit {
 
   settingsChanged(event?:any){
     if (this.settings.bAutoIncrementBagNumbers) {
-      this.item.sBagNumber = (event) ? event : this.settings?.sPrefix + (this.settings?.nLastBagNumber + 1).toString();
+      this.item.sBagNumber = (event) ? event : (this.settings?.sPrefix || '') + (this.settings?.nLastBagNumber + 1).toString();
       this.itemChanged.emit({type:'settingsChanged', data: this.item.sBagNumber});
     }
   }
