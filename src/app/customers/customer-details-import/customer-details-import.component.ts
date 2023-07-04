7ed49c98124d1231735e8c80578fdf0eb6bdc13f
bharti sarvaiya <bharti.devmonk@gmail.com>
2023-07-04 18:27:21 +0530

    customer import changes.

--- src/app/customers/customer-details-import/customer-details-import.component.ts
+++ src/app/customers/customer-details-import/customer-details-import.component.ts
@@ -57,7 +57,7 @@ export class CustomerDetailsImportComponent implements OnInit, OnChanges {
       this.headerOptions = Object.keys(this.parsedCustomerData[0]);
       this.customerDetailsForm = {};
       this.updateTemplateForm = {};
-      this.headerOptions.filter((option: any) => this.updateTemplateForm[option] = 'overwrite');
+      this.headerOptions.filter((option: any) => this.updateTemplateForm[option] = '');
       this.getDynamicFields(false);
     }
   }
