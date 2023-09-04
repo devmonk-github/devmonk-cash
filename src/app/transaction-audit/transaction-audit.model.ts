7bf190040e9b662584ea05327678c2ea865d45e9
Naimish Kher <naimishkher95@gmail.com>
2023-09-04 11:38:05 +0530

    View mode added for the migrated statistics

--- src/app/transaction-audit/transaction-audit.model.ts
+++ src/app/transaction-audit/transaction-audit.model.ts
@@ -2,17 +2,20 @@ export interface View {
     sKey: string;
     sValue: string;
     children: ViewMenuChild[];
+    bDisable: boolean;
 }
 
 export interface ViewMenuChild {
     sKey?: string;
     sValue?: string;
     children: ChildChild[];
+    bDisable: boolean
 }
 
 export interface ChildChild {
     sKey: string;
     sValue: string;
+    bDisable: boolean;
     data?: {
         displayMethod?: eDisplayMethodKeysEnum,
         modeFilter?: 'supplier' | 'businessOwner',
