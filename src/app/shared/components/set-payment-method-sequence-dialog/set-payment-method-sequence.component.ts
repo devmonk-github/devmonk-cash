8bcae278bdb51da4493286a99ed4e3c75f3ae9d1
EmanuelaG <emanuelaretailgear@gmail.com>
2023-09-22 15:19:37 +0200

    Default payment method logic changed. + save sequence fix

--- src/app/shared/components/set-payment-method-sequence-dialog/set-payment-method-sequence.component.ts
+++ src/app/shared/components/set-payment-method-sequence-dialog/set-payment-method-sequence.component.ts
@@ -34,14 +34,27 @@ export class SetPaymentMethodSequenceComponent implements OnInit {
 
   ngOnInit() {
     // console.log(this.payMethods)
-    if (!this.aPaymentMethodSequence?.length) this.aPaymentMethodSequence = this.payMethods.map((el: any) => el._id)
-    else {
-      const aPaymentMethods:any = []
-      this.aPaymentMethodSequence.forEach((iPaymentMethodId:any) => {
-        aPaymentMethods.push(this.payMethods.find((el: any) => el._id === iPaymentMethodId))
-      })
-      this.payMethods = aPaymentMethods;
-    }
+    this.aPaymentMethodSequence = this.payMethods.map((el: any) => el._id);
+
+    /*OLD LOGIC*/
+    /* - Not showing new P.M. which has the toggle "show in cash register" enabled.
+    *  - Not removing the P.M. which has the toggle "show in cash register" disabled.
+    *  - What is the purpose to pass this.aPaymentMethodSequence ? If user expect to see on the modal
+    *    those P.M. which have "show in cash register" enabled.
+    *  - Lines of code, inside else block, is pushing also undefined element, which cause errors on cash register.
+    */
+
+    // if (!this.aPaymentMethodSequence?.length){
+    //   this.aPaymentMethodSequence = this.payMethods.map((el: any) => el._id)
+    // } 
+    // else {
+    //   const aPaymentMethods:any = [];
+    //   this.aPaymentMethodSequence.forEach((iPaymentMethodId:any) => {
+    //     aPaymentMethods.push(this.payMethods.find((el: any) => el._id === iPaymentMethodId))
+    //   })
+    //   this.payMethods = aPaymentMethods;
+    //   console.log("aPaymentMethods", this.payMethods)
+    // }
   }
 
   drop(event: CdkDragDrop<string[]>) {
